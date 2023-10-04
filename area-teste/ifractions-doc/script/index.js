2cb9afd7ead14cd5998437451b4d5020f175d324
lairaalmas <lairaalmas@gmail.com>
2023-10-04 00:05:29 -0300

    refactor>: update and fix ifractins documentation

--- area-teste/ifractions-doc/script/index.js
+++ area-teste/ifractions-doc/script/index.js
@@ -48,12 +48,12 @@ nav.innerHTML = `
             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
               <li><a class="dropdown-item" target="_self" href='./paginas/documentacao.html'>Início</a>
               </li>
-              <li><a class="dropdown-item" target="_self" href='./paginas/area_professor/page_2.html'>Sou Professor</a>
+              <li><a class="dropdown-item" target="_self" href='./paginas/documentacao.html#sou-prof'>Sou Professor</a>
               </li>
-              <li><a class="dropdown-item" target="_self" href='./paginas/area_professor/page_1.html'>Sou
+              <li><a class="dropdown-item" target="_self" href='./paginas/documentacao.html#sou-admin'>Sou
                   Administrador Moodle</a>
               </li>
-              <li><a class="dropdown-item" target="_self" href='./paginas/area_programador/page_1.html'>Sou
+              <li><a class="dropdown-item" target="_self" href='./paginas/documentacao.html#sou-dev'>Sou
                   Desenvolvedor</a>
               </li>
             </ul>
@@ -82,25 +82,77 @@ span.onclick = () => (modal.style.display = "none");
 
 // Gera indice
 const toc = document.querySelector(".ifr__toc");
-const sections_all = document.querySelectorAll("section");
-sections_all.forEach((section_h2) => {
-  const h2 = section_h2.querySelectorAll("h2")[0];
-  if (h2) {
-    const h2_ul = document.createElement("ul");
-    const h2_li = document.createElement("li");
-    const h3_ul = document.createElement("ul");
-    const sections_h3 = section_h2.querySelectorAll("section");
-    sections_h3.forEach((section_h3) => {
-      const h3 = section_h3.querySelectorAll("h3")[0];
-      if (h3) {
-        const h3_li = document.createElement("li");
-        h3_li.innerHTML = `<a href="#${section_h3.id}" target="_self">${h3.innerHTML}</a>`;
-        h3_ul.appendChild(h3_li);
-      }
-    });
-    h2_li.innerHTML = `<a href="#${section_h2.id}" target="_self">${h2.innerHTML}</a>`;
-    h2_ul.appendChild(h2_li);
-    h2_ul.appendChild(h3_ul);
-    toc.appendChild(h2_ul);
-  }
-});
+if (toc) {
+  const sections_all = document.querySelectorAll("section");
+  sections_all.forEach((section_h2) => {
+    const h2 = section_h2.querySelectorAll("h2")[0];
+    if (h2) {
+      const h2_ul = document.createElement("ul");
+      const h2_li = document.createElement("li");
+      const h3_ul = document.createElement("ul");
+      const sections_h3 = section_h2.querySelectorAll("section");
+      sections_h3.forEach((section_h3) => {
+        const h3 = section_h3.querySelectorAll("h3")[0];
+        if (h3) {
+          const h3_li = document.createElement("li");
+          h3_li.innerHTML = `<a href="#${section_h3.id}" target="_self">${h3.innerHTML}</a>`;
+          h3_ul.appendChild(h3_li);
+        }
+      });
+      h2_li.innerHTML = `<a href="#${section_h2.id}" target="_self">${h2.innerHTML}</a>`;
+      h2_ul.appendChild(h2_li);
+      h2_ul.appendChild(h3_ul);
+      toc.appendChild(h2_ul);
+    }
+  });
+}
+
+// Insere em todos rodapés
+const updateMenu = (depth) => {
+  const path = depth == 1 ? "../" : "../../";
+  const semiPath = depth == 1 ? "./" : "../";
+  nav.innerHTML = `
+ <div class="container-fluid">
+      <a class="navbar-brand" aria-current="page" target="_self" href="${path}index.html">iFractions</a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" target="_self" href="${path}index.html#download">Download</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" target="_self" href="${semiPath}sobre/page_1.html">Conhecendo o iFractions</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+              data-bs-toggle="dropdown" aria-expanded="false"> Documentação
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" target="_self" href='${semiPath}documentacao.html'>Início</a>
+              </li>
+              <li><a class="dropdown-item" target="_self" href='${semiPath}documentacao.html#sou-prof'>Sou Professor</a>
+              </li>
+              <li><a class="dropdown-item" target="_self" href='${semiPath}documentacao.html#sou-dev'>Sou
+                  Administrador Moodle</a>
+              </li>
+              <li><a class="dropdown-item" target="_self" href='${semiPath}documentacao.html#sou-admin'>Sou
+                  Desenvolvedor</a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" target="_self" href="${semiPath}publicacoes.html">Publicações</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+`;
+};
